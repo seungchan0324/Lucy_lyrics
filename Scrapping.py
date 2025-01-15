dfd4dff2dae681703da153a32c70da6bacbf631b
seungchan0324 <winchan324@gmail.com>
2025-01-15 18:13:33 +0900

    add release date

--- Scrapping.py
+++ Scrapping.py
@@ -2,3 +2,5 @@ from Scrapping_lyrics import Extractor_Lyrics
 
 extractor = Extractor_Lyrics()
 extractor.start_crawling()
+# release_data = extractor.find_release_date()
+# print(release_data["DEAR."])
--- Scrapping_lyrics.py
+++ Scrapping_lyrics.py
@@ -18,7 +18,8 @@ class Extractor_Lyrics:
 
     def start_crawling(self):
         track_urls = self.track_url_crawling()
-        lyrics = self.lyrics_scrapping(track_urls)
+        album_data = self.find_release_date()
+        lyrics = self.lyrics_scrapping(track_urls, album_data)
         self.save_to_file("Lucy", lyrics)
 
     def wait(self, seconds=4):
@@ -42,7 +43,7 @@ class Extractor_Lyrics:
                 track_hrefs.append(track_href)
         return track_hrefs
 
-    def lyrics_scrapping(self, urls):
+    def lyrics_scrapping(self, urls, album_data):
         lyrics_info = []
         for url in urls:
             response = requests.get(
@@ -52,6 +53,7 @@ class Extractor_Lyrics:
                 },
             )
             soup = BeautifulSoup(response.content, "html.parser")
+
             title = soup.find("header", class_="pgTitle").find("h1").text
             if "Inst" in title or "inst" in title:
                 continue
@@ -61,15 +63,41 @@ class Extractor_Lyrics:
                 soup.find("div", class_="basicInfo").find_all("tr")[2].find("a").text
             )
             like = soup.find("a", class_="like").find("em").text
+
+            release_date = album_data[album]
+
             lyrics_info.append(
                 {
                     "title": title,
                     "lyric": lyric,
                     "album": album,
                     "like": like,
+                    "release_date": release_date,
                 }
             )
         return lyrics_info
+
+    def find_release_date(self):
+        types = ["RELEASE", "JOIN", "COMPIL"]
+        album_data = {}
+        for type in types:
+            url = f"https://music.bugs.co.kr/artist/80332208/albums?type={type}"
+            response = requests.get(
+                url,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+                },
+            )
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            albums = soup.find("ul", class_="albumList").find_all("li")
+
+            for album in albums:
+                album_title = album.find("div", class_="albumTitle").find("a").text
+                album_release_date = album.find("time").text
+                album_data[album_title] = album_release_date
+
+        return album_data
 
     def save_to_file(self, file_name, lyrics):
         file = open(f"{file_name}.csv", "w", encoding="utf-8", newline="")
